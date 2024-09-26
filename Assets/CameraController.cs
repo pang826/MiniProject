5f4ea561254bcfe5725f3f2b2542e3e7cade3421
ogh826 <eys555@naver.com>
2024-09-26 12:00:23 +0900

    [Chore] 카메라 건물 투과 기능 구현

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,14 +4,58 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] GameObject target;
     [SerializeField] Vector3 offset;
-
+    Vector3 screenCenter;
+    Ray ray;
+    Renderer obj;
     private void Awake()
     {
+        screenCenter = new Vector2(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
+    }
+
+    private void Start()
+    {
+        
     }
     private void Update()
     {
-        transform.position = target.position + offset;
+        FollowPlayer();
+        
+        
+    }
+    private void FixedUpdate()
+    {
+        SeeThroughBuilding();
+    }
+    void FollowPlayer()
+    {
+        transform.position = target.transform.position + offset;
+    }
+
+    // 건물투시
+    void SeeThroughBuilding()
+    {
+        ray = Camera.main.ScreenPointToRay(screenCenter);
+
+        Physics.Raycast(ray, out RaycastHit hit, 100);
+
+        if (hit.collider != target.GetComponent<Collider>())
+        {
+            obj = hit.collider.gameObject.GetComponent<MeshRenderer>();
+            obj.enabled = false;
+
+            //hit.collider.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            
+            Debug.Log(hit.collider.gameObject.name);
+        }
+        if(hit.collider == target.GetComponent<Collider>())
+        {
+            obj.enabled = true;
+            //hit.collider.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+        
+            //meshRenderer.enabled = true;
+        
     }
 }
