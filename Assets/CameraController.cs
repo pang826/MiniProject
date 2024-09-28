ce7a5b23c49ddca511c96c654a3418f732ee0ebd
ogh826 <eys555@naver.com>
2024-09-29 00:35:22 +0900

    [Refact] 카메라 투시 기능 리팩토링
    애셋들의 경우 단일 오브젝트가 아닌 자식오브젝트를 포함하는 경우가 많기 때문에 모든 매터리얼을 투명화 하기 위하여 코드 리팩토링을 진행

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -8,10 +6,10 @@ public class CameraController : MonoBehaviour
     [SerializeField] Vector3 offset;
     [SerializeField] LayerMask layerMask;
     Vector3 screenCenter;
-    //Renderer obj;
-
-    [SerializeField] MeshRenderer meshRenderer;
-    [SerializeField] Material buildingMaterial;
+    //[SerializeField] Material buildingMaterial;
+    [SerializeField] MeshRenderer[] meshs;
+    [SerializeField] Material[] materials;
+    int size;
 
     private void Awake()
     {
@@ -37,17 +35,49 @@ public class CameraController : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(screenCenter);
 
-        // layerMask로 설정한 layer에 raycast가 닿을 경우 해당 오브젝트의 매터리얼 투명도를 0.1f로 설정
-        if(Physics.Raycast(ray, out RaycastHit hit, 100, layerMask))
+        //// layerMask로 설정한 layer에 raycast가 닿을 경우 해당 오브젝트의 매터리얼 투명도를 0.1f로 설정
+        //if (Physics.Raycast(ray, out RaycastHit hit, 100, layerMask))
+        //{
+        //    buildingMaterial = hit.collider.gameObject.GetComponent<MeshRenderer>().material;
+        //    buildingMaterial.color = new Color(buildingMaterial.color.r, buildingMaterial.color.g, buildingMaterial.color.b, 0.1f);
+        //}
+        //// 해당 오브젝트가 정해져있고 그 오브젝트가 0.1f의 투명도를 가졌을 경우 raycast에 닿지 않으면 투명도 1로 복구
+        //// 조건 뒷부분을 사용하지 않으면 무한히 실행하는 문제가 있음
+        //else if (buildingMaterial != null && buildingMaterial.color.a == 0.1f)
+        //{
+        //    buildingMaterial.color = new Color(buildingMaterial.color.r, buildingMaterial.color.g, buildingMaterial.color.b, 1f);
+        //}
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, layerMask))
         {
-            buildingMaterial = hit.collider.gameObject.GetComponent<MeshRenderer>().material;
-            buildingMaterial.color = new Color(buildingMaterial.color.r, buildingMaterial.color.g, buildingMaterial.color.b, 0.1f);
+            meshs = hit.collider.gameObject.GetComponentsInChildren<MeshRenderer>();
+            size = meshs.Length;
+            materials = new Material[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                materials[i] = meshs[i].material;
+            }
+            for (int j = 0; j < size; j++)
+            {
+                materials[j].color = new Color(materials[j].color.r, materials[j].color.g, materials[j].color.b, 0.1f);
+            }
+
+            //foreach (Material material in materials)
+            //{
+            //    material.color = new Color(material.color.r, material.color.g, material.color.b, 0.1f);
+            //}
         }
-        // 해당 오브젝트가 정해져있고 그 오브젝트가 0.1f의 투명도를 가졌을 경우 raycast에 닿지 않으면 투명도 1로 복구
-        // 조건 뒷부분을 사용하지 않으면 무한히 실행하는 문제가 있음
-        else if(buildingMaterial != null && buildingMaterial.color.a == 0.1f)
+        else if (materials != null)
         {
-            buildingMaterial.color = new Color(buildingMaterial.color.r, buildingMaterial.color.g, buildingMaterial.color.b, 1f);
+            // foreach (Material material in materials)
+            // {
+            //     material.color = new Color(material.color.r, material.color.g, material.color.b, 1f);
+            // }
+            
+            for (int x = 0; x < size; x++)
+            {
+                materials[x].color = new Color(materials[x].color.r, materials[x].color.g, materials[x].color.b, 1f);
+            }
         }
     }
 }
