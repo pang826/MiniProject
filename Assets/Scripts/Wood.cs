7d61365d1dd97fbce86eb5891a7e132f8dfa990a
ogh826 <eys555@naver.com>
2024-10-01 21:49:31 +0900

    [Feat] 아이템 감지 기능 구현

--- Assets/Scripts/Wood.cs
+++ Assets/Scripts/Wood.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 public class Wood : MonoBehaviour
 {
     [SerializeField] ItemData data; // 이름, 무게, 타입(무기, 치료, 재료)
     [SerializeField] ItemData.Type type;
-    public TextMeshProUGUI nameTmp;
-    public TextMeshProUGUI weightTmp;
-    public TextMeshProUGUI typeTmp;
+    public TextMeshProUGUI itemTmp;
+    public ScrollRect itemSpotUi;
+    public RectTransform itemSpot;
     private void Start()
     {
         type = data.type;
-        
+        itemSpotUi = GameObject.FindGameObjectWithTag("ItemSpot").GetComponent<ScrollRect>();
+        itemSpot = itemSpotUi.content;
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            itemTmp = itemSpot.AddComponent<TextMeshProUGUI>();
+        }
+    }
     private void OnTriggerStay(Collider other)
     {
         if(other.tag == "Player")
         {
-            nameTmp.text = data.itemName;
-            weightTmp.text = data.Weight.ToString();
-            typeTmp.text = this.type.ToString();
+            itemTmp.text = $"{data.itemName}\t\t{data.Weight.ToString()}\t\t{type.ToString()}";
         }
     }
 
@@ -29,9 +37,7 @@ public class Wood : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            nameTmp.text = null;
-            weightTmp.text = null;
-            typeTmp.text = null;
+            Destroy(itemTmp);
         }
     }
 }
